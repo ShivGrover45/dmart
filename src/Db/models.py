c27c95a775e120a165bf8c87e57aa8617fcfd5de
ShivGrover45 <codershiv16@gmail.com>
2026-07-18 17:35:43 +0530

    added name to password col

--- src/Db/models.py
+++ src/Db/models.py
@@ -8,7 +8,7 @@ class User(Base):
     __tablename__="users"
     id=Column(Integer,primary_key=True,index=True)
     username=Column(String,index=True,unique=True,nullable=False)
-    hashed_password=Column(String,nullable=False)
+    hashed_password=Column(String,nullable=False,name="password")
     role=Column(String,nullable=False)
 
 class Product(Base):

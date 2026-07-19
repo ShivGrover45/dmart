f4b3eb2fc6609a0db62908d9398a5afce4796d89
ShivGrover45 <codershiv16@gmail.com>
2026-07-19 11:31:45 +0530

    token added for authorization

--- src/Router/auth.py
+++ src/Router/auth.py
@@ -5,12 +5,13 @@ from Db.database import get_db
 from Schemas.schema import UserCreate,UserResponse,LoginRequest
 
 
+
 router=APIRouter()
 
 @router.post('/signup',response_model=UserResponse)
 def create_user(user:UserCreate,db:Session=Depends(get_db)):
     return  signup(user,db)
 
-@router.post('/login',response_model=UserResponse)
+@router.post('/login')
 def sign_in(user:LoginRequest,db:Session=Depends(get_db)):
     return signin(user,db=db)
--- src/controller/auth.py
+++ src/controller/auth.py
@@ -2,6 +2,14 @@ from sqlalchemy.orm import Session
 from Db.models import User
 from pwdlib import PasswordHash
 from fastapi import HTTPException
+from dotenv import load_dotenv
+from datetime import datetime,timezone,timedelta
+import jwt
+import os 
+
+load_dotenv()
+
+jwt_secret=os.getenv("JWT_SECRET")
 
 password_hash=PasswordHash.recommended()
 
@@ -24,8 +32,18 @@ def signin(user_data,db:Session):
     password=user_data.password
     user=db.query(User).filter(User.username==username).first()
     user_password=password_hash.verify(password=password,hash=user.hashed_password)
+  
     if not user:
         raise HTTPException(status_code=401,detail="Invalid Username or Password")
     if not user_password:
         raise HTTPException(status_code=401,detail="Invalid username or password")
-    return user
+    payload={
+        "id":user.id,
+        "role":user.role,
+        "exp":datetime.now(timezone.utc) + timedelta(hours=1)
+    }
+    encoded=jwt.encode(payload=payload,key=jwt_secret,algorithm="HS256")
+    return {
+        "access_token":encoded,
+        "token_type":"bearer"
+    }
